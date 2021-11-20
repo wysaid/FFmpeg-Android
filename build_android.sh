adbddcfd632c5620ebfd36363b16df8216784a52
wangyang <this@wysaid.org>
2021-11-20 15:26:13 +0800

    fix git url

--- build_android.sh
+++ build_android.sh
@@ -1,38 +1,41 @@
 #!/usr/bin/env bash
 
 if [[ -z "$NDK" ]]; then
- echo "NDK variable not set, please do 'export NDK=/path/of/ndk-bundle'"
+    echo "NDK variable not set, please do 'export NDK=/path/of/ndk-bundle'"
 fi
 
-THIS_DIR=`cd $(dirname "$0"); pwd`
+THIS_DIR=$(
+    cd $(dirname "$0")
+    pwd
+)
 echo "THIS_DIR=$THIS_DIR"
 cd $THIS_DIR
 
-export PREFIX=`pwd`/build
+export PREFIX=$(pwd)/build
 export SONAME=libffmpeg.so
 
 echo NDK=${NDK}
 echo PREFIX=${PREFIX}
 
 if [[ ! -d "${THIS_DIR}/x264" ]]; then
-	echo "cloning x264..."
-	if [[ "$1" == "--enable-gitee" ]]; then
-		git clone git@gitee.com:wysaid/x264.git --recursive
-	else
-		echo "use '--enable-gitee' to clone with gitee. (maybe faster with gitee in Asia)"
-		git clone https://code.videolan.org/videolan/x264.git --recursive
-	fi
-	
+    echo "cloning x264..."
+    if [[ "$1" == "--enable-gitee" ]]; then
+        git clone https://gitee.com/wysaid/x264.git --recursive
+    else
+        echo "use '--enable-gitee' to clone with gitee. (maybe faster with gitee in Asia)"
+        git clone https://code.videolan.org/videolan/x264.git --recursive
+    fi
+
 fi
 
 if [[ ! -d "${THIS_DIR}/ffmpeg" ]]; then
-	echo "cloning ffmpeg..."
-	if [[ "$1" == "--enable-gitee" ]]; then
-		git clone git@gitee.com:ChinaFFmpeg/ffmpeg.git ffmpeg --recursive
-	else
-		echo "use '--enable-gitee' to clone with gitee. (maybe faster with gitee in Asia)"
-		git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg --recursive
-	fi
+    echo "cloning ffmpeg..."
+    if [[ "$1" == "--enable-gitee" ]]; then
+        git clone https://gitee.com/ChinaFFmpeg/ffmpeg.git ffmpeg --recursive
+    else
+        echo "use '--enable-gitee' to clone with gitee. (maybe faster with gitee in Asia)"
+        git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg --recursive
+    fi
 fi
 
 cd $THIS_DIR/x264
