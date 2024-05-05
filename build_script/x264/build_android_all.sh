54f1e565ccafc2c342c6bdc82eea14d8cca2268c
wangyang <wangyang@kuaishou.com>
2024-05-05 16:50:37 +0800

    update build script

--- build_script/x264/build_android_all.sh
+++ build_script/x264/build_android_all.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ -z "$1" ]]; then
-  echo "Invalid argument!"
-  exit 1
+    echo "Invalid argument!"
+    exit 1
 fi
 
 THIS_DIR=$(
-  cd $(dirname "$0")
-  pwd
+    cd $(dirname "$0")
+    pwd
 )
 
 X264_DIR=$1
@@ -18,19 +18,13 @@ cd $X264_DIR
 git clean -fdx
 
 # Build arm v6 v7a
-bash $THIS_DIR/build_android_arm.sh "$X264_DIR"
+bash $THIS_DIR/build_android_arm.sh "$X264_DIR" || exit 1
 
 # Build arm64 v8a
-bash $THIS_DIR/build_android_arm64-v8a.sh "$X264_DIR"
-
-# Build mips
-# bash $THIS_DIR/build_android_mips.sh "$X264_DIR"
-
-# Build mips64
-# bash $THIS_DIR/build_android_mips64.sh "$X264_DIR"
+bash $THIS_DIR/build_android_arm64-v8a.sh "$X264_DIR" || exit 1
 
 # Build x86
-bash $THIS_DIR/build_android_x86.sh "$X264_DIR"
+bash $THIS_DIR/build_android_x86.sh "$X264_DIR" || exit 1
 
 # Build x86_64
-bash $THIS_DIR/build_android_x86_64.sh "$X264_DIR"
+bash $THIS_DIR/build_android_x86_64.sh "$X264_DIR" || exit 1
